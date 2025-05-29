a78ba19809b6e135d616d5d7d7869939378514ca
OTG12 <oteneezekiel04@gmail.com>
2025-05-29 13:10:42 +0100

    OTG3

--- src/componets/Certificate.jsx
+++ src/componets/Certificate.jsx
@@ -1,29 +1,35 @@
-import React, { useEffect } from 'react'
-import Cr from '../assets/Cr.jpeg'
+import React, { useEffect } from 'react';
+import Cr from '../assets/Cr.jpeg';
 import AOS from 'aos';
-import 'aos/dist/aos.css'
+import 'aos/dist/aos.css';
 
 const Certificate = () => {
-     useEffect(() => {
-        AOS.init({
-          offset: 200,
-          duration: 800,
-          easing: 'ease-in-sine',
-          delay: 100,
-        })
-      }, [])
-  return (
-    <div id='Certificate'>
-        <div>
-            <h1 data-aos='zoom-in'className="font-bold text-center text-3xl underline">Certificate</h1>
-        </div>
-        <div>
-            <img src={Cr}  alt="Skills image"
-                className="w-400 h-200 px-9 md:w-full h-full"
-                data-aos="zoom-in"/>
-        </div>
-    </div>
-  )
-}
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+  }, []);
 
-export default Certificate
+  return (
+    <div id='Certificate' className="w-full py-10 px-5 md:px-20">
+      <div className="mb-8">
+        <h1 data-aos='zoom-in' className="font-bold text-center text-3xl md:text-4xl underline">
+          Certificate
+        </h1>
+      </div>
+      <div className="flex justify-center items-center">
+        <img
+          src={Cr}
+          alt="Skills image"
+          className="w-full max-w-[800px] h-auto rounded-lg shadow-lg"
+          data-aos="zoom-in"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Certificate;
